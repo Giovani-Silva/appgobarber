037da81d6c95dd1517283b7e919a808062f41000
Giovani Silva <giovani.silva@live.com>
2020-11-07 15:47:47 -0300

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  globals: {
+    __DEV__: 'readonly',
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, {extensions: ['.tsx']}],
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
 };
