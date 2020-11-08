876d7cbee5645f2143efbe58f5b2d56d888bfe89
Giovani Silva <giovani.silva@live.com>
2020-11-08 16:30:40 -0300

    splash screen

--- src/pages/SigIn/index.tsx
+++ src/pages/SigIn/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-import {Container} from './styles';
+import {Container, Title} from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SigIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>Faça o seu logon</Title>
     </Container>
   );
 };
