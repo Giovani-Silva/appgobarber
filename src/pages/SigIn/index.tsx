8b97e356cb55db7e56d9f98138db7747df8ca7e9
Giovani Silva <giovani.silva@live.com>
2020-11-14 20:18:06 -0300

    signin connect api

--- src/pages/SigIn/index.tsx
+++ src/pages/SigIn/index.tsx
@@ -1,13 +1,28 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
-import { Form } from '@unform/mobile'
 import * as Yup from 'yup';
 
-import logoImg from '../../assets/logo.png';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import { useAuth } from '../../hooks/auth';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
 
 import {
   Container,
@@ -15,23 +30,23 @@ import {
   ForgotPassword,
   ForgotPasswordText,
   CreateAccountButton,
-  CreateAccountText,
+  CreateAccountButtonText,
 } from './styles';
-import { FormHandles } from '@unform/core';
-import getValidationErrors from '../../utils/getValidationErrors';
 
 interface SignInFormData {
   email: string;
   password: string;
 }
 
-
-const SigIn: React.FC = () => {
+const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
+
   const navigation = useNavigation();
 
-    const handleSignIn = useCallback(
+  const { signIn } = useAuth();
+
+  const handleSignIn = useCallback(
     async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({});
@@ -47,14 +62,15 @@ const SigIn: React.FC = () => {
           abortEarly: false,
         });
 
-        // await signIn({
-        //   email: data.email,
-        //   password: data.password,
-        // });
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
 
+          console.log(errors);
 
           formRef.current?.setErrors(errors);
 
@@ -67,73 +83,75 @@ const SigIn: React.FC = () => {
         );
       }
     },
-    [],
+    [signIn],
   );
-  
+
   return (
     <>
-      <KeyboardAvoidingView 
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-        style={{flex:1}}
         enabled
       >
-        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{flex:1}}>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
           <Container>
             <Image source={logoImg} />
+
             <View>
-              <Title>Faça o seu logon</Title>
+              <Title>Faça seu logon</Title>
             </View>
+
             <Form ref={formRef} onSubmit={handleSignIn}>
-              <Input 
+              <Input
                 autoCorrect={false}
                 autoCapitalize="none"
                 keyboardType="email-address"
-                name="email" 
-                icon="mail" 
-                placeholder="E-mail" 
-                returnKeyType="next" 
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
                 onSubmitEditing={() => {
                   passwordInputRef.current?.focus();
                 }}
               />
+
               <Input
                 ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
                 secureTextEntry
-                returnKeyType="send" 
-                name="password" 
-                icon="lock" 
-                placeholder="Senha" 
-                 onSubmitEditing={() => {
+                returnKeyType="send"
+                onSubmitEditing={() => {
                   formRef.current?.submitForm();
                 }}
               />
 
               <Button
                 onPress={() => {
-                  formRef.current?.submitForm()
+                  formRef.current?.submitForm();
                 }}
-                >
+              >
                 Entrar
               </Button>
             </Form>
 
-
-            <ForgotPassword
-              onPress={() => {
-                console.log('clicou');
-              }}
-            >
+            <ForgotPassword onPress={() => {}}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
             </ForgotPassword>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
+
       <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
-        <CreateAccountText>Criar uma conta</CreateAccountText>
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
     </>
   );
 };
 
-export default SigIn;
+export default SignIn;
