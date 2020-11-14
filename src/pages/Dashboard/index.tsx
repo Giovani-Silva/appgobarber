4c510ff881cc58d671d954593d50b6c6faae3ad9
Giovani Silva <giovani.silva@live.com>
2020-11-14 20:56:09 -0300

    logout dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {View, Button} from 'react-native';
 import {
   Container
 } from './styles';
+import { useAuth } from '../../hooks/auth';
 
-const Dashboard: React.FC = () => <Container />
 
+const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
+
+return (
+  <Container>
+    <Button title="logout" onPress={signOut} />
+  </Container>
+)
+}
 export default Dashboard;
